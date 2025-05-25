80df972c6f7770026602aa6c607d69596436b45a
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-05-25 23:50:55 +0100

    correction of simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,13 +1,9 @@
-# Define the numbers
-number1 = 10
-number2 = 5
+# simple_interest.py
 
-# Perform arithmetic operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+principal = 1000
+rate = 0.05
+time = 3
 
-# Print the results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
