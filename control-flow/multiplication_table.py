20c54a084408951b5fbeea85d017be97868baecc
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-06-01 21:38:55 +0100

    correction of number of iterations for multiplications in multoplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@
 number = float (input("Enter a number to see its multiplication table:"))
 
 #For loop to get the multipllication table up to 10
-for i in range(1,6):
+for i in range(1,11):
     multiplication = number * i
     print (f"{number}*{i}={multiplication}")
