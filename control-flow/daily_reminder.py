23690e731724c0811b887b3f4dbe794a05a69200
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-06-01 21:42:59 +0100

    correction of punctuation to correspond to checker error

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,21 @@
-#Task description; priority and time bounds
-task = input ("Enter your task:")
-priority = input ("Priority (high/medium/low)")
-time_bound = input ("Is it time-bound? (yes/no)")
+# Task description; priority and time bounds
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
-match priority :
-    case "high" :
-        if time_bound== "yes" : 
-            print (f" {task} is a high priority task that requires immediate attention today!")
-        else :
-            print (f"{task} is a high priority task that requires completion as soon as possible")
-    case "medium" :
-        if time_bound== "yes" : 
-            print (f" {task} is a medium priority task that requires immediate attention today!")
-        else :
-            print (f"{task} is a medium priority task that requires completion as soon as  no high priority and time bound tasks are done")
-    case "low" :
-        if time_bound== "yes" : 
-            print (f" {task} is a low priority task that requires immediate attention today!")
-        else :
-            print (f"{task} is a low priority task that requires completion as soon as  no high and medium priority and time bound tasks are done")            
-    
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"{task} is a high priority task that requires immediate attention today!")
+        else:
+            print(f"{task} is a high priority task that requires completion as soon as possible.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"{task} is a medium priority task that requires immediate attention today!")
+        else:
+            print(f"{task} is a medium priority task that should be done after all high priority time-bound tasks.")
+    case "low":
+        if time_bound == "yes":
+            print(f"{task} is a low priority task that requires attention today, but only after higher priorities.")
+        else:
+            print(f"{task} is a low priority task to be done when no high or medium priority time-bound tasks remain.")
