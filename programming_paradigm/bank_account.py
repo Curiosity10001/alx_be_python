4416cb14b165e2dde0632329b352781a1bc50fc5
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-08-25 18:12:17 +0100

    V3 of bank_account.py correction of synthax for if statements, self call added for class attributes, print statement correction, spaces instead of tabs)

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,16 @@
-class BankAccount: #Creates empty non active class ; this line alone is like a empty placeholder
-	def __init__(self, initial_balance = 0): #intial balance 0
-		self.account_balance = initial_balance #creating the modifiable parameter and assining the initial balance to it
+class BankAccount:  # Creates empty non active class ; this line alone is like a empty placeholder
+    def __init__(self, initial_balance=0):  # initial balance 0
+        self.account_balance = initial_balance  # create the attribute and assign initial balance to it
 
-	def deposit(self,amount):
-		account_balance += amount
+    def deposit(self, amount):
+        self.account_balance += amount
 
-	def withdraw(self,amount):
-		if account_balance - amount>=0 :
-		account_balance -= amount
-		return True
-		else False
+    def withdraw(self, amount):
+        if self.account_balance - amount >= 0:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
 
-	def display_balance(self):
-		print(f'Current Balance: $'{self._account_balance})
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
