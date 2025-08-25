af3e89236d005911a1de021bf5ba61b7fdecd90f
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-08-25 18:16:33 +0100

    bank_account.py V4 corrected output hard 2 digits after comma'

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:  # Creates empty non active class ; this line alone is like a
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
