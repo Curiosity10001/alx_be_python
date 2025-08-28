99dee8a5aee54a26d4cd5b78d1a097c178fb1890
Imane Bourouyne <imane.bourouyne@gmail.com>
2025-08-29 00:30:04 +0100

    V2 space correction for checker spelling precision

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,4 +1,4 @@
-class Book :
+class Book:
     def __init__(self,title,author,is_checked_out = False):
         self.title = title
         self.author = author
@@ -10,7 +10,7 @@ class Book :
      def book_is_returned(self):
         self._is_checked_out = False
         
-class Library :
+class Library:
     def __init__(self):
         self._books = []
     
